1a29fef0400d1c0fc608faef34c71a23f76f1bef
Sarvarbek Tilobov <sarvarbektilobov@gmail.com>
2024-04-04 15:26:19 +0500

    upload 04/04/2024

--- document 25/app.js
+++ document 25/app.js
@@ -1689,39 +1689,109 @@
 // }
 // my().then((data0) => { return data0 }).catch((data1) => { return data1 })
 
-const container = document.querySelector('.container');
-const base = 'https://fakestoreapi.com';
+// const container = document.querySelector('.container');
+// const base = 'https://fakestoreapi.com';
+// function f() {
+//     return fetch(`${base}/products`)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             fr(data)
+//         });
+// }
+// function fr(it) {
+//     it.forEach(e => {
+//         const box = document.createElement('div')
+//         const img = document.createElement('img')
+//         const newItem = document.createElement('div')
+//         const ds = document.createElement('div')
+//         box.setAttribute('class', 'card bg-primary mt-5 w-25 p-1')
+//         img.setAttribute('src', `${e.image}`)
+//         newItem.innerHTML = `<h5>${e.title}</h5>`
+//         ds.innerHTML = `<p>${e.description}</p>`
+//         ds.setAttribute('class', 'ds')
+//         box.append(img)
+//         box.append(newItem)
+//         box.append(ds)
+//         container.append(box)
+//     });
+// }
+// f();
+
+// const container = document.querySelector('.container');
+// const data_base = 'fakestoreapi';
+
+// const f = () => fetch(`${data_base}/products`).then((data1) => data1.json().then((data2) => fR(data2)));
+// let data = function (data) return data.forEach(e => {});
+
+// const person = {
+//     isName: 'Sarvarbek',
+//     age: 16,
+//     id: 15,
+// }
+// function f() {
+//     console.log(`isName: ${this.isName}, age:  ${this.age}, job: ${this.id}`);
+// }
+// // f.call({ isName: `Sarvar`, age: `16`, id: `1` })
+// f.call(person)
+
+const list = [
+    {
+        "id": 7,
+        "email": "michael.lawson@reqres.in",
+        "first_name": "Michael",
+        "last_name": "Lawson",
+        "avatar": "https://reqres.in/img/faces/7-image.jpg"
+    },
+    {
+        "id": 8,
+        "email": "lindsay.ferguson@reqres.in",
+        "first_name": "Lindsay",
+        "last_name": "Ferguson",
+        "avatar": "https://reqres.in/img/faces/8-image.jpg"
+    },
+    {
+        "id": 9,
+        "email": "tobias.funke@reqres.in",
+        "first_name": "Tobias",
+        "last_name": "Funke",
+        "avatar": "https://reqres.in/img/faces/9-image.jpg"
+    },
+    {
+        "id": 10,
+        "email": "byron.fields@reqres.in",
+        "first_name": "Byron",
+        "last_name": "Fields",
+        "avatar": "https://reqres.in/img/faces/10-image.jpg"
+    },
+    {
+        "id": 11,
+        "email": "george.edwards@reqres.in",
+        "first_name": "George",
+        "last_name": "Edwards",
+        "avatar": "https://reqres.in/img/faces/11-image.jpg"
+    },
+    {
+        "id": 12,
+        "email": "rachel.howell@reqres.in",
+        "first_name": "Rachel",
+        "last_name": "Howell",
+        "avatar": "https://reqres.in/img/faces/12-image.jpg"
+    }
+]
+function start() {
+    console.log(`isName: ${this.first_name} last-name: ${this.last_name}, email: ${this.email}`);
+}
+
+// for (let i of list) {
+//     start.call(i)
+// }
 
 function f() {
-    return fetch(`${base}/products`)
-        .then((res) => res.json())
-        .then((data) => {
-            fr(data)
-        });
-}
-
-function fr(it) {
-    it.forEach(e => {
-        console.log(e);
-        const box = document.createElement('div')
-        const img = document.createElement('img')
-        const newItem = document.createElement('div')
-        const ds = document.createElement('div')
-        box.setAttribute('class', 'card bg-primary mt-5 w-25 p-1')
-        img.setAttribute('src', `${e.image}`)
-        newItem.innerHTML = `<h5>${e.title}</h5>`
-        ds.innerHTML = `<p>${e.description}</p>`
-        ds.setAttribute('class', 'ds')
-        box.append(img)
-        box.append(newItem)
-        box.append(ds)
-        container.append(box)
+    list.forEach(e => {
+        console.log(`last name :${e.last_name},first name :${e.first_name} email: ${e.email}`);
     });
 }
-f();
-
-
-
+f()
 
 
 
