065aed18d244bb6834eeeb1a6c2721e976e75c0e
Sarvarbek Tilobov <sarvarbektilobov@gmail.com>
2024-04-04 17:37:15 +0500

    upload04/04/2024

--- document 25/app.js
+++ document 25/app.js
@@ -1729,69 +1729,46 @@
 //     id: 15,
 // }
 // function f() {
-//     console.log(`isName: ${this.isName}, age:  ${this.age}, job: ${this.id}`);
+//     console.log(`isName: ${this.isName}, age:  ${this.age}, id: ${this.id}`);
 // }
 // // f.call({ isName: `Sarvar`, age: `16`, id: `1` })
 // f.call(person)
 
-const list = [
-    {
-        "id": 7,
-        "email": "michael.lawson@reqres.in",
-        "first_name": "Michael",
-        "last_name": "Lawson",
-        "avatar": "https://reqres.in/img/faces/7-image.jpg"
-    },
-    {
-        "id": 8,
-        "email": "lindsay.ferguson@reqres.in",
-        "first_name": "Lindsay",
-        "last_name": "Ferguson",
-        "avatar": "https://reqres.in/img/faces/8-image.jpg"
-    },
-    {
-        "id": 9,
-        "email": "tobias.funke@reqres.in",
-        "first_name": "Tobias",
-        "last_name": "Funke",
-        "avatar": "https://reqres.in/img/faces/9-image.jpg"
-    },
-    {
-        "id": 10,
-        "email": "byron.fields@reqres.in",
-        "first_name": "Byron",
-        "last_name": "Fields",
-        "avatar": "https://reqres.in/img/faces/10-image.jpg"
-    },
-    {
-        "id": 11,
-        "email": "george.edwards@reqres.in",
-        "first_name": "George",
-        "last_name": "Edwards",
-        "avatar": "https://reqres.in/img/faces/11-image.jpg"
-    },
-    {
-        "id": 12,
-        "email": "rachel.howell@reqres.in",
-        "first_name": "Rachel",
-        "last_name": "Howell",
-        "avatar": "https://reqres.in/img/faces/12-image.jpg"
-    }
-]
-function start() {
-    console.log(`isName: ${this.first_name} last-name: ${this.last_name}, email: ${this.email}`);
-}
-
+// const list = [
+//     {
+//         "id": 7,
+//         "email": "michael.lawson@reqres.in",
+//         "first_name": "Michael",
+//         "last_name": "Lawson",
+//         "avatar": "https://reqres.in/img/faces/7-image.jpg"
+//     },
+//     {
+//         "id": 8,
+//         "email": "lindsay.ferguson@reqres.in",
+//         "first_name": "Lindsay",
+//         "last_name": "Ferguson",
+//         "avatar": "https://reqres.in/img/faces/8-image.jpg"
+//     },
+//     {
+//         "id": 9,
+//         "email": "tobias.funke@reqres.in",
+//         "first_name": "Tobias",
+//         "last_name": "Funke",
+//         "avatar": "https://reqres.in/img/faces/9-image.jpg"
+//     },
+// ]
+// function start() {
+//     console.log(`isName: ${this.first_name} last-name: ${this.last_name}, email: ${this.email}`);
+// }
 // for (let i of list) {
 //     start.call(i)
 // }
+// // list.forEach(e => {
+// //     console.log(`last-name :${e.last_name},first-name :${e.first_name} email: ${e.email}`);
+// // });
 
-function f() {
-    list.forEach(e => {
-        console.log(`last name :${e.last_name},first name :${e.first_name} email: ${e.email}`);
-    });
-}
-f()
+
+
 
 
 
